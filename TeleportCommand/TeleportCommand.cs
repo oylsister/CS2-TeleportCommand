f0f887af46f5b39a3d3305c8f99192fba9b482c7
Prajna Paramahansa <satriagama1@gmail.com>
2023-11-24 11:17:06 +0800

    fix: fix NullException after execute command and arguments counter

--- TeleportCommand/TeleportCommand.cs
+++ TeleportCommand/TeleportCommand.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using CounterStrikeSharp.API;
-using CounterStrikeSharp.API.Core;
+﻿using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Utils;
@@ -14,7 +12,7 @@ public class TeleportCommand : BasePlugin
     public override string ModuleAuthor => "Oylsister";
     public override string ModuleDescription => "Advanced Teleport Command for Counter-Strike 2";
 
-    private FindTargetModule _findTarget;
+    private readonly FindTargetModule _findTarget;
 
     public TeleportCommand()
     {
@@ -31,17 +29,17 @@ public class TeleportCommand : BasePlugin
     [RequiresPermissions("@css/slay")]
     private void Command_Teleport(CCSPlayerController? client, CommandInfo info)
     {
-        if (info.ArgCount <= 0) 
+        if (info.ArgCount <= 2)
         {
             info.ReplyToCommand("[Teleport] Usage: css_teleport <client> <dest-client>");
             return;
         }
 
         // Get the client name that you want to be teleported.
-        List<CCSPlayerController> targetname = _findTarget.FindTarget(client!, info.GetArg(0));
+        var targetname = _findTarget.FindTarget(client, info.GetArg(1));
 
         // Get the client name that you want to be destination.
-        List<CCSPlayerController> destTemp = _findTarget.FindTarget(client!, info.GetArg(1), true);
+        var destTemp = _findTarget.FindTarget(client, info.GetArg(2), true);
 
         if (targetname.Count <= 0)
         {
@@ -56,35 +54,35 @@ public class TeleportCommand : BasePlugin
         }
 
         // target destination can only be one.
-        CCSPlayerController targetdestination = destTemp[0];
+        var targetdestination = destTemp[0];
 
         // Find the angle and position.
-        CCSPlayerPawn playerPawn = targetdestination.PlayerPawn.Value;
-        Vector Position = playerPawn.AbsOrigin!;
-        QAngle Angle = playerPawn.AbsRotation!;
+        var playerPawn = targetdestination.PlayerPawn.Value;
+        var position = playerPawn.AbsOrigin ?? new Vector(default);
+        var angle = playerPawn.AbsRotation ?? new QAngle(default);
+        var velocity = playerPawn.AbsVelocity;
 
-        foreach(var player in targetname)
+        foreach (var targetPawn in targetname.Select(player => player.PlayerPawn.Value))
         {
-            // Teleport all of them
-            CCSPlayerPawn targetPawn = player.PlayerPawn.Value;
-            targetPawn.Teleport(Position, Angle, new Vector(0f, 0f, 0f));
+            targetPawn.Teleport(position, angle, velocity);
         }
 
         info.ReplyToCommand("[Teleport] Successfully Teleport Client.");
-        return;
     }
 
     [RequiresPermissions("@css/slay")]
     private void Command_Bring(CCSPlayerController? client, CommandInfo info)
     {
-        if (info.ArgCount <= 0)
+        if (info.ArgCount <= 1)
         {
             info.ReplyToCommand("[Teleport] Usage: css_bring <client>");
             return;
         }
 
+
         // Get the client name that you want to be teleported.
-        List<CCSPlayerController> targetname = _findTarget.FindTarget(client!, info.GetArg(0));
+        var target = info.GetArg(1);
+        var targetname = _findTarget.FindTarget(client, target);
 
         if (targetname.Count <= 0)
         {
@@ -93,18 +91,16 @@ public class TeleportCommand : BasePlugin
         }
 
         // Find the angle and position.
-        CCSPlayerPawn playerPawn = client!.PlayerPawn.Value;
-        Vector Position = playerPawn.AbsOrigin!;
-        QAngle Angle = playerPawn.AbsRotation!;
+        var playerPawn = client?.PlayerPawn.Value;
+        var position = playerPawn?.AbsOrigin;
+        var angle = playerPawn?.AbsRotation;
+        var velocity = playerPawn?.AbsVelocity;
 
-        foreach (var player in targetname)
+        foreach (var targetPawn in targetname.Select(player => player.PlayerPawn.Value))
         {
-            // Teleport all of them
-            CCSPlayerPawn targetPawn = player.PlayerPawn.Value;
-            targetPawn.Teleport(Position, Angle, new Vector(0f, 0f, 0f));
+            var name = targetPawn.Controller.Value.PlayerName;
+            targetPawn.Teleport(position!, angle!, velocity!);
+            info.ReplyToCommand($"[Teleport] Successfully Teleport bring { name } to you.");
         }
-
-        info.ReplyToCommand("[Teleport] Successfully Teleport bring client to you.");
-        return;
     }
 }
